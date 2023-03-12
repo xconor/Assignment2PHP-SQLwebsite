900fb0801b303b27b9b339c66b095bcfb03850a3
xconor <d00261104@student.dkit.ie>
2023-03-12 17:52:28 +0000

    added contact form files

--- page-3.php
+++ page-3.php
@@ -2,9 +2,25 @@
 
 <main class="container">
   <div class="starter-template text-center">
-    <h1>Page 3</h1>
+  <h1>Contact us</h1>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
   </div>
+
+<form method="POST" name="contactform" action="contact-form-handler.php"> 
+<p>
+<label for='name'>Your Name:</label> <br>
+<input type="text" name="name">
+</p>
+<p>
+<label for='email'>Email Address:</label> <br>
+<input type="text" name="email"> <br>
+</p>
+<p>
+<label for='message'>Message:</label> <br>
+<textarea name="message"></textarea>
+</p>
+<input type="submit" value="Submit"><br>
+</form>
 
 </main><!-- /.container -->
 <?php include 'includes/footer.php';?>
